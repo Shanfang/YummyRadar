ec5679d9961c1b9d775fe46a590abfa81393f47a
Shanfang <shanfangzhao@gmail.com>
2018-03-24 10:24:27 -0400

    adding analysis component

--- YummyRadar/src/app/app.module.ts
+++ YummyRadar/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalysisComponent
   ],
   imports: [
     BrowserModule
