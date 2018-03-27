1046c815dc601a2fec7250f84da1aa1f47202cc5
Shanfang <shanfangzhao@gmail.com>
2018-03-27 17:40:40 -0400

    update app rendering for newly added analysis component

--- YummyRadar/src/app/app.module.ts
+++ YummyRadar/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AnalysisComponent } from './analysis/analysis.component';
+import { AnalysisAreaComponent } from './analysis/analysis-area/analysis-area.component';
+import { AnalysisTypeComponent } from './analysis/analysis-type/analysis-type.component';
 // import { AppRoutingModule } from './app-routing.module';
 // import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnalysisComponent
+    AnalysisComponent,
+    AnalysisAreaComponent,
+    AnalysisTypeComponent
   ],
   imports: [
     // AppRoutingModule,
