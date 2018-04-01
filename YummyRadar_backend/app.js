b7696aeeedcbad6941b43d05d5a69cf173afc5e2
Shanfang <shanfangzhao@gmail.com>
2018-04-01 18:13:05 -0400

    separate routes from app module

--- YummyRadar_backend/app.js
+++ YummyRadar_backend/app.js
@@ -1,59 +1,20 @@
 var express = require('express');
-var oracledb = require('oracledb');
-var dbConfig = require('./dbconfig');
+var bodyParser = require('body-parser');
+
+var appRoutes = require('./routes/app');
+var analysisRoutes = require('./routes/analysis');
 
 const app = express();
 const PORT = 3000;
 
-// Get a non-pooled connection
-oracledb.getConnection(
-  {
-    user          : dbConfig.user,
-    password      : dbConfig.password,
-    connectString : dbConfig.connectString
-  },
-  function(err, connection)
-  {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
-    connection.execute(
-      `SELECT *
-       FROM professors
-       WHERE id = :id`,
-      [1],
-      { maxRows: 1
-      },
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-      // The callback function handles the SQL execution results
-      function(err, result)
-      {
-        if (err) {
-          console.error(err.message);
-          doRelease(connection);
-          return;
-        }
-        console.log(result.metaData); 
-        console.log(result.rows);     
-        doRelease(connection);
-      });
-  });
+// app.get('/', (req, res) => 
+//     res.send(`Thanks for visiting YummyRadar!`));
 
-// Note: connections should always be released when not needed
-function doRelease(connection)
-{
-  connection.close(
-    function(err) {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-}
-
-app.get('/', (req, res) => 
-    res.send(`Node server is running on port ${PORT}`)
-);
+app.use('/analysis', analysisRoutes);
+app.use('/', appRoutes);
 
 app.listen(PORT, () =>
     console.log(`Server is listening on port ${PORT}`)
