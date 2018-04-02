1e1ecd96c0972316594c1f0024b0d22181faa7f7
Shanfang <shanfangzhao@gmail.com>
2018-04-02 19:46:52 -0400

    sketch and test analysis location component

--- YummyRadar/src/app/components/analysis-area/analysis-area.component.ts
+++ YummyRadar/src/app/components/analysis-area/analysis-area.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalysisService } from '../../Services/analysis.service';
+import { Response } from '@angular/http';
+import { Location } from '../../modules/location.module';
 
 @Component({
   selector: 'app-analysis-area',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./analysis-area.component.css']
 })
 export class AnalysisAreaComponent implements OnInit {
-
-  constructor() { }
-
+  // restaurants = [
+  //   {
+  //     id: this.generateID(),
+  //     name: 'YummyCity',
+  //     rating: 10
+  //   },
+  //   {
+  //     id: this.generateID(),
+  //     name: 'SmallHouse',
+  //     rating: 5
+  //   }
+  // ];
+  constructor(private analysisService: AnalysisService) {}
   ngOnInit() {
   }
 
+  onSelectLocation() {
+    this.analysisService.getRestaurants(location)
+    .subscribe(
+      (response: Response) => {
+        const data = response.json();
+        console.log(data);
+      },
+      (error) => console.log(error)
+    )
+  }
+  // onAddRestaurant(name: string) {
+  //   this.restaurants.push({
+  //     id: this.generateID(),
+  //     name: name,
+  //     rating: 6
+  //   });
+  // }
+
+  // onSave() {
+  //   this.analysisService.storeRestaurant(this.restaurants)
+  //   .subscribe(
+  //     (response: Response) => {
+  //       const data = response.json();
+  //       console.log(data);
+  //     },
+  //     (error) => console.log(error)
+  //   );
+  // }
+
+  // private generateID() {
+  //   return Math.round(Math.random() * 100);
+  // }
 }
