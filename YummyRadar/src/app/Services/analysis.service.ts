8930f78fa1108bbdbb36f23b2a44db59c3f537e6
Shanfang <shanfangzhao@gmail.com>
2018-04-02 19:47:56 -0400

    add analysis service

---
+++ YummyRadar/src/app/Services/analysis.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Location } from '../modules/location.module';
+
+@Injectable()
+export class AnalysisService {
+    constructor(private http: Http) {}
+    apiURL: string = "http://127.0.0.1:3000";
+    storeRestaurant(restaurants: any[]) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post(`${this.apiURL}/api/addRest`, restaurants, {headers: headers});
+    }
+    getRestaurants(location: Location) {
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post(`${this.apiURL}/api/area`, location, {headers: headers});
+    }
+}
