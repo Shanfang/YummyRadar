36ff12b9fb5a865a7819c2c206fa0faf55e7b287
Shanfang <shanfangzhao@gmail.com>
2018-04-02 19:42:21 -0400

    resolve CORS issue

--- YummyRadar_backend/app.js
+++ YummyRadar_backend/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var appRoutes = require('./routes/app');
 var analysisRoutes = require('./routes/analysis');
@@ -7,14 +8,15 @@ var analysisRoutes = require('./routes/analysis');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// app.get('/', (req, res) => 
-//     res.send(`Thanks for visiting YummyRadar!`));
+app.post('/api/addRest', (req, res, next) => 
+    console.log(req.body));
 
-app.use('/analysis', analysisRoutes);
-app.use('/', appRoutes);
+// app.use('/analysis', analysisRoutes);
+// app.use('/', appRoutes);
 
 app.listen(PORT, () =>
     console.log(`Server is listening on port ${PORT}`)
