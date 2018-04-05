0553eb39647af629450885b50948ba6b9dd07460
jmyu1994 <jmyu1994@gmail.com>
2018-04-05 02:27:22 -0400

    ignore node_modules

---
+++ YummyRadar_backend/loadData/loadData.js
@@ -0,0 +1,42 @@
+// myscript.js
+
+var oracledb = require('oracledb');
+
+oracledb.getConnection(
+    {
+        user          : "jingmin",
+        password      : "jmyu1994",
+        connectString : "oracle.cise.ufl.edu:1521/orcl"
+    },
+    function(err, connection)
+    {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        connection.execute(
+            'select * from country',  // bind value for :id
+            function(err, result)
+            {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                console.log(result.rows);
+                doRelease(connection);
+            });
+
+    });
+
+function doRelease(connection)
+{
+    connection.close(
+        function(err) {
+            if (err)
+                console.error(err.message);
+        });
+}
+
+// check whether ignore works
+// push to jimmy branch
