cf1ea8b64461d925dd3d9224b192f5048835ee62
Shanfang <shanfangzhao@gmail.com>
2018-04-20 17:09:00 -0400

    remove data loading files

--- loader/mysql-export.sql
+++
@@ -1,5 +0,0 @@
-SELECT id,name,neighborhood,address,city,state,postal_code,latitude,longitude,stars,review_count,is_open 
-INTO OUTFILE '/Users/shanfang/desktop/result.csv'
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-FROM yelp_db.business 
